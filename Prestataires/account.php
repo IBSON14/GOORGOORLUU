9fe9e9d48177e44a2d1c69db9f41a437cb4c54d9
IBSON14 <ib.thiamass@gmail.com>
2017-03-18 22:41:57 +0000

    prestataires

---
+++ Prestataires/account.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>COMPTE PRESTATAIRE</title>
+	<link rel="stylesheet" type="text/css" href="../css/style1.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/accountstyle.css"/>
+	<!--[if lt IE 7]>
+	<link rel="stylesheet" href="ie.css" type="text/css">
+	<![endif]-->
+</head>
+<body>
+	<div id="wrap">
+
+		<div id="header">
+			<p><img src="images/logo.png" style="float: right;margin: 0.5em 2em;"></p>
+		</div>
+
+		<div id="content-wrap">
+			<div id="content">
+				<ul class="menu-vertical">
+					<li><a href="#">PRESTATAIRE</a></li>
+					<li ><a href="">Notification</a></li>
+					<li ><a href="">Account</a></li>
+					<li><a href="">Modifier</a></li>
+					<li><a href="">Statistique</a></li>
+				</ul>
+				<div class="pres">
+					PRESENTATION
+				</div>	
+			</div>
+		</div>
+
+		<div id="footer">
+			<div id="labfooter">
+				<p><a href="http://www.456bereastreet.com/lab/">a propos</a> | <a href="http://www.456bereastreet.com/">Conditions d'utilisations</a> | <a href="http://www.456bereastreet.com/lab/">confidentialite</a> </p>
+			</div>	
+		</div>
+
+	</div>
+</body>
+</html>
